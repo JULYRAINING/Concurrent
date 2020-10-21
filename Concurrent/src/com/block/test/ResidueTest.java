c282c2cd33dcee245bb406b87ac91a758f247209
julyraining <julyraining@163.com>
2020-10-21 23:33:22 +0800

    调整目录结构

--- Concurrent/src/com/block/test/ResidueTest.java
+++
@@ -1,8 +0,0 @@
-package com.block.test;
-
-public class ResidueTest {
-	public static void main(String[] args) {
-		
-		System.out.println(17);
-	}
-}
--- Concurrent/src/com/pool/factory/ThreadFactory.java
+++
@@ -1,5 +0,0 @@
-package com.pool.factory;
-
-public interface ThreadFactory {
-	Thread newThread(Runnable r);
-}
